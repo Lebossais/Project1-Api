34270d9311e93d677eae77e8641f1fe5d2ef1552
Benjamin De Coster <BenjaminDC59@gmail.com>
2024-01-29 15:26:07 +0100

    29/01/2024 - First commit

---
+++ src/test/java/com/loparok/partagerecette/PartageRecetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loparok.partagerecette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartageRecetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
